dee9c68c4363bafb9696310c574156cf71b7164e
Максим Вовкович <vovkovihc542@gmail.com>
2023-08-23 13:01:53 +0300

    .... + mark-as and show

--- FIrstModule/FIrstModule/Program.cs
+++ FIrstModule/FIrstModule/Program.cs
@@ -1,28 +1,9 @@
-﻿using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
-
-void AddItem(string[] items)
-{
-   
-}
-void RemoveItem(string[] items)
-{
-
-}
-void MarkAs(string[] items)
-{
-
-}
-void Show(string[] items)
-{
-
-}
-
-
+﻿
 Console.WriteLine("Hello, input the command for your tasks.");
-List<string> items = new List<string>() {"str","add-item","make a homework"};
+List<string> items = new List<string>() { "str", "add-item", "make a homework" };
 while (true)
 {
+    Console.WriteLine(Environment.NewLine);
     Console.WriteLine(">add-item");
     Console.WriteLine(">remove-item");
     Console.WriteLine(">mark-as");
@@ -38,14 +19,14 @@ while (true)
                 Console.WriteLine("Input a task:");
                 var task = Console.ReadLine();
 
-                if (task != null)
+                if (!string.IsNullOrEmpty(task))
                 {
                     if (!items.Any())
                     {
-                        items.Add(task);break;
+                        items.Add(task); break;
                     }
                     bool IsTask = true;
-                    foreach(string item in items)
+                    foreach (string item in items)
                     {
                         if (task.ToLower().Contains(item.ToLower()))
                         {
@@ -80,11 +61,11 @@ while (true)
                 Console.WriteLine(Environment.NewLine);
                 Console.WriteLine("What you want to delete?");
                 var delete = Console.ReadLine();
-                if (delete != null)
+                if (!string.IsNullOrEmpty(delete))
                 {
-                    if(delete == "*")
+                    if (delete == "*")
                     {
-                        items.Clear();break;
+                        items.Clear(); break;
                     }
                     bool IsTask = true;
                     foreach (string item in items)
@@ -100,10 +81,13 @@ while (true)
                         items.Remove(delete);
                         break;
                     }
-                    else { items.Remove(delete);
+                    else
+                    {
+                        items.Remove(delete);
                         Console.WriteLine(Environment.NewLine);
                         Console.WriteLine("This task isn`t already");
-                        break; }
+                        break;
+                    }
                 }
                 else
                 {
@@ -113,16 +97,116 @@ while (true)
             }
         case "mark-as":
             {
-                 break;
+                Console.WriteLine("Input a staus:");
+                string? status = Console.ReadLine();
+
+                if (status == "1" || status == "0")
+                {
+                    Console.WriteLine("Input a task:");
+                    var task = Console.ReadLine();
+                    if (!string.IsNullOrEmpty(task))
+                    {
+                        for (int i = 0; i < items.Count; i++)
+                        {
+                            if (task.ToLower().Contains(items[i].ToLower()))
+                            {
+                                items[i] = string.Join(" ", status, items[i]);
+                                if (status == "0")
+                                {
+                                    int indexOfComma = items[i].IndexOf(',');
+                                    if (indexOfComma != -1)
+                                    {
+                                        items[i] = items[i].Substring(0, indexOfComma).Trim(); break;
+                                    }
+                                }
+                                if (status == "1")
+                                {
+                                    var date = Console.ReadLine();
+                                    if (!string.IsNullOrEmpty(date))
+                                    {
+                                        if (DateTime.TryParse(date, out DateTime executionTime))
+                                        {
+                                            items[i] = string.Join(", ", items[i], executionTime.ToString("yyyy-MM-dd")); break;
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("Invalid time format. Using current time."); break;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        items[i] = string.Join(", ", items[i], DateTime.Now.ToString("yyyy-MM-dd")); break;
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Input error task.Try again.");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(Environment.NewLine);
+                    Console.WriteLine("Input error status.Try again.");
+                    break;
+                }
+                break;
             }
         case "show":
             {
-                 break;
+                Console.WriteLine("Input a staus:");
+                string? status = Console.ReadLine();
+                if (status == "1" || status == "0" || status == "")
+                {
+                    if (status == "1")
+                    {
+                        for (int i = 0; i < items.Count; i++)
+                        {
+                            int indexOfStatus = items[i].IndexOf('1');
+                            if (indexOfStatus != -1)
+                                if (indexOfStatus != -1)
+                                {
+                                    Console.WriteLine(items[i]);
+                                }
+                        }
+                        break;
+                    }
+                    else if (status == "0")
+                    {
+                        for (int i = 0; i < items.Count; i++)
+                        {
+                            int indexOfStatus = items[i].IndexOf('0');
+                            if (indexOfStatus != -1)
+                            {
+                                Console.WriteLine(items[i]);
+                            }
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        foreach (var item in items)
+                        {
+                            Console.WriteLine(item.ToString());
+                        }
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(Environment.NewLine);
+                    Console.WriteLine("Input error status.Try again.");
+                    break;
+                }
             }
         case "exit":
             {
-                 return;
+                return;
             }
-        default: {  return; }
+        default: { return; }
     }
 }
