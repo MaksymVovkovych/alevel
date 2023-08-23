8ba5dcf0754fade5ff4a49b732b615c1b27da005
Максим Вовкович <vovkovihc542@gmail.com>
2023-08-23 11:11:14 +0300

    add-item and remove-item

--- FIrstModule/FIrstModule/Program.cs
+++ FIrstModule/FIrstModule/Program.cs
@@ -1,29 +1,26 @@
-﻿void AddItem(string[,] items)
-{
-    Console.WriteLine("Input your item");
-    string item = Console.ReadLine().ToLower();
-    if (item != null)
-    {
+﻿using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 
-    }
-    else Console.WriteLine("String is null.");
+void AddItem(string[] items)
+{
+   
 }
-void RemoveItem(string[,] items)
+void RemoveItem(string[] items)
 {
 
 }
-void MarkAs(string[,] items)
+void MarkAs(string[] items)
 {
 
 }
-void Show(string[,] items)
+void Show(string[] items)
 {
 
 }
 
 
 Console.WriteLine("Hello, input the command for your tasks.");
-
+List<string> items = new List<string>() {"str","add-item","make a homework"};
 while (true)
 {
     Console.WriteLine(">add-item");
@@ -31,28 +28,96 @@ while (true)
     Console.WriteLine(">mark-as");
     Console.WriteLine(">show");
     Console.WriteLine(">exit");
-
     Console.WriteLine("Input a command:");
     var command = Console.ReadLine();
-
+    Console.WriteLine(Environment.NewLine);
     switch (command)
     {
         case "add-item":
             {
-                AddItem(items);
-                break;
+                Console.WriteLine("Input a task:");
+                var task = Console.ReadLine();
+
+                if (task != null)
+                {
+                    if (!items.Any())
+                    {
+                        items.Add(task);break;
+                    }
+                    bool IsTask = true;
+                    foreach(string item in items)
+                    {
+                        if (task.ToLower().Contains(item.ToLower()))
+                        {
+                            IsTask = false;
+                            break;
+                        }
+                    }
+                    if (!IsTask)
+                    {
+                        Console.WriteLine(Environment.NewLine);
+                        Console.WriteLine("This task is already"); break;
+                    }
+                    else { items.Add(task); break; }
+                }
+                else
+                {
+                    Console.WriteLine(Environment.NewLine);
+                    Console.WriteLine("Error.. You input anything! Try again"); break;
+                }
             }
         case "remove-item":
             {
-                RemoveItem(items); break;
+                if (!items.Any())
+                {
+                    Console.WriteLine(Environment.NewLine);
+                    Console.WriteLine("There aren`t tasks"); break;
+                }
+                foreach (string item in items)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine(Environment.NewLine);
+                Console.WriteLine("What you want to delete?");
+                var delete = Console.ReadLine();
+                if (delete != null)
+                {
+                    if(delete == "*")
+                    {
+                        items.Clear();break;
+                    }
+                    bool IsTask = true;
+                    foreach (string item in items)
+                    {
+                        if (delete.ToLower().Contains(item.ToLower()))
+                        {
+                            IsTask = false;
+                            break;
+                        }
+                    }
+                    if (!IsTask)
+                    {
+                        items.Remove(delete);
+                        break;
+                    }
+                    else { items.Remove(delete);
+                        Console.WriteLine(Environment.NewLine);
+                        Console.WriteLine("This task isn`t already");
+                        break; }
+                }
+                else
+                {
+                    Console.WriteLine(Environment.NewLine);
+                    Console.WriteLine("Error.. You input anything! Try again"); break;
+                }
             }
         case "mark-as":
             {
-                MarkAs(item); break;
+                 break;
             }
         case "show":
             {
-                Show(items); break;
+                 break;
             }
         case "exit":
             {
