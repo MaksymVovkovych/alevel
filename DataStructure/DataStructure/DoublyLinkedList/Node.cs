08ce458ccad6b12696cc289f53ff6758460a8bd8
Максим Вовкович <vovkovihc542@gmail.com>
2023-09-27 17:11:00 +0300

    BinaryTree and DLL

---
+++ DataStructure/DataStructure/DoublyLinkedList/Node.cs
@@ -0,0 +1,15 @@
+﻿namespace DataStructure.DoublyLinkedList
+{
+    public class Node<T>
+    {
+        public T value { get; set; }
+        public Node<T>? Prev { get; set; }
+        public Node<T>? Next { get; set; }
+        public Node(T value)
+        {
+            this.value = value;
+            Prev = null;
+            Next = null;
+        }
+    }
+}
