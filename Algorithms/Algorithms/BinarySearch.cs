b7ca17dcd0b1cf4937f299b3b7a2ad119810a691
Максим Вовкович <vovkovihc542@gmail.com>
2023-09-29 00:53:29 +0300

    Factorial,Fibonachi,QuickSort and BinarySearch

---
+++ Algorithms/Algorithms/BinarySearch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public static class BinarySearch
+    {
+        public static int BinarySearchFunc(int[] sortedArray , int target)
+        {
+            int left = 0;
+            int right = sortedArray.Length - 1;
+            while (left <= right)
+            {
+                int middle = left + (right - left) / 2;
+                if (sortedArray[middle] == target)
+                    return middle;
+                else if (sortedArray[middle] < target)
+                {
+                    left = middle + 1;
+                }
+                else
+                {
+                    right = middle - 1;
+                }
+            }
+            return -1;
+            
+        }
+    }
+}
---
+++ Algorithms/Algorithms/Fibonachi.cs
@@ -0,0 +1,10 @@
+﻿namespace Algorithms
+{
+    public static class Fibonachi
+    {
+        public static int FibonachiFunc(this int n)
+        {
+           return n <= 1 ? n : FibonachiFunc(n - 1) + FibonachiFunc(n - 2);
+        }
+    }
+}
