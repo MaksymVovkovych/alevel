6bfc4f09d1244062e08a5e4a949286460007db60
Максим Вовкович <vovkovihc542@gmail.com>
2023-10-05 12:41:43 +0300

    hello+world async

---
+++ Tasks/Tasks/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Microsoft.VisualBasic;
+using System.Runtime.CompilerServices;
+using Tasks;
+
+class Program
+{
+   
+     static void Main()
+    {
+        Example1.Run();
+        Example2.RunAsync();
+    }
+    
+}
